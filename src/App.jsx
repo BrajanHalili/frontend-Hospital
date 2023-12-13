1d616e4d90db29a977f6646e91dd397355e1ec74
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-12-13 01:06:39 -0500

    created update patient form
    -Created Update patient form
    -Created routes for update forms for patient, doctor, and appointment.
    
    NEXT STEPS:
    -finalize "update entry" get post methods in back end to link to front end form.
    -once update patient is working, create respective front end forms for doctor and appointment

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,13 @@ import ApptDirectory from "./routes/appointment/ApptDirectory";
 import StartPage from "./routes/StartPage";
 
 
-import UpdatePatient from "./routes/patient/AddPatientPage";
 import PatientDetails from "./routes/patient/PatientDetails";
 import DoctorDetails from "./routes/doctor/DoctorDetails";
 import ApptDetails from "./routes/appointment/ApptDetails";
 
-import { PatientsContextProvider } from "./context/PatientsContext";
 import Layout from "./components/Layout";
 import AddPatientPage from "./routes/patient/AddPatientPage";
 import AddDoctorPage from "./routes/doctor/AddDoctorPage";
-import AddAppointmentPage from "./routes/appointment/AddApptPage";
 import AddApptPage from "./routes/appointment/AddApptPage";
 
 
@@ -37,9 +34,9 @@ const App = () => {
                         <Route path="/doctors" element={<DoctorDirectory />} />
                         <Route path="/appointments" element={<ApptDirectory />} />
                     </Route>
-                    <Route exact path="/patients/id" element={<PatientDetails />} />
-                    <Route exact path="/doctors/id" element={<DoctorDetails />} />
-                    <Route exact path="/appointments/id" element={<ApptDetails />} />
+                    <Route exact path="/patients/:id" element={<PatientDetails />} />
+                    <Route exact path="/doctors/:id" element={<DoctorDetails />} />
+                    <Route exact path="/appointments/:id" element={<ApptDetails />} />
                     <Route exact path="/patients/add" element={<AddPatientPage />} />
                     <Route exact path="/doctors/add" element={<AddDoctorPage />} />
                     <Route exact path="/appointments/add" element={<AddApptPage />} />
