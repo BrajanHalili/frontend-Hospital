97ae0f06d6729a13c7fea64da0d8814ae060dfe0
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-12-16 19:04:10 -0500

    adding patient update slice

--- src/context/AppointmentStore.js
+++ src/context/AppointmentStore.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import patientDataReducer from './PatientSlice';
 import doctorDataReducer from './DoctorSlice';
 import updateDoctorDataReducer from './UpdateDoctorSlice'
+import updatePatientDatareducer from './UpdatePatientSlice'
 
 
 export default configureStore({
@@ -9,5 +10,6 @@ export default configureStore({
         patientData: patientDataReducer,
         doctorData: doctorDataReducer,
         updateDoctorData: updateDoctorDataReducer,
+        updatePatientData: updatePatientDatareducer,
     }
 })
